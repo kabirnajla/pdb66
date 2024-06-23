104d53b46a1ce3ff4f9af64792b12edb04a04565
Node.js-connect-Mysql <54183497+kabirnajla@users.noreply.github.com>
2024-06-24 01:42:20 +0700

    https://github.com/s6411423101/pdb66.git

--- components/LookerDashboard.tsx
+++ components/LookerDashboard.tsx
@@ -7,18 +7,14 @@ interface LookerDashboardProps {
 const LookerDashboard: React.FC<LookerDashboardProps> = ({ className }) => {
   return (
     <div className={className}>
-      <iframe
-        title="Looker Dashboard"
-        style={{
+      <iframe title="Looker Dashboard" style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
           height: "100%",
           border: "none",
-        }}
-        src="https://lookerstudio.google.com/embed/reporting/f1837c75-6dd4-4355-9966-ca1785d14302/page/4OrWB"
-      ></iframe>
+        }} src="https://lookerstudio.google.com/embed/reporting/d3fcf77a-2aac-4cb8-bcaa-606ed68ca89e/page/5Y23D" sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
     </div>
   );
 };
